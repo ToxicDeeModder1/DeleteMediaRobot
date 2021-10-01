78f182c06aa64b9a1a84c757d383a4491438445f
Toxic Dee Modder <90701056+ToxicDeeModder1@users.noreply.github.com>
2021-10-01 03:02:43 +0200

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,12 +1,12 @@
-#----------------------------------- https://github.com/m4mallu/DeleteMediaRobot -------------------------------------#
+#----------------------------------- https://github.com/ToxicDeeModder1/DeleteMediaRobot -------------------------------------#
 
 class Presets(object):
     WELCOME_TEXT = "Hello.. <b>{} 👋</b>\n\n<code>I can delete medias in Your Group !\n\n" \
                    "Just.. add me as as admin with all privileges in your Group !\n\n" \
                    "Then.. Send </code><b> /cleanmedia</b><code> to delete !</code>\n\n" \
-                   "<b>My Author is: </b><a href='https://t.me/space4renjith'>                HERE</a>\n" \
-                   "<b>My code can be seen: </b><a href='https://github.com/m4mallu/DeleteMediaRobot'> HERE</a>"
+                   "<b>My Author is: </b><a href='https://t.me/ToxicDeeModderr'>                HERE</a>\n" \
+                   "<b>My code can be seen: </b><a href='https://t.me/ToxicDeeModderr'> HERE</a>"
     FILE_TYPES = ["document", "video", "audio", "photo", "sticker", "voice", "animation"]
     NOT_HERE_TEXT = "<b>Not Here</b> 😂\n\n<code>In your group where i am an admin</code> 🤷"
     DELETING_MESSAGE = "<b>Deleting Medias...</b>"
-    DELETED_MESSAGE = "<b><a href='https://github.com/m4mallu/DeleteMediaRobot'>Medias Deleted</a></b>"
+    DELETED_MESSAGE = "<b><a href='https://github.com/ToxicDeeModder1/DeleteMediaRobot'>Medias Deleted</a></b>"
